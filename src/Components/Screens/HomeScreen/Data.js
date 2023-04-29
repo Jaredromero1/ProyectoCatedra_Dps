32ed33383bd61c50ba731a2ffdaad7162f4806ea
Jaredromero1 <jaredromero08@gmail.com>
2023-04-28 18:47:03 -0600

    update Api

--- src/Components/Screens/HomeScreen/Data.js
+++ src/Components/Screens/HomeScreen/Data.js
@@ -6,24 +6,28 @@ export default[
         banner: require("../../../../assets/MCimg.png"),
         menu: [
             {
+                id: 1,
                 name: "Big Mac",
                 description: "Soda lata 355ml y papas medianas",
                 price: 7.35,
                 image: "../../../../assets/"
             },
-            {
+            {   
+                id: 2,
                 name: "Triple Bacon",
                 description: "Soda lata 355ml y papas medianas",
                 price: 7.00,
                 image: "../../../../assets/"
             },
             {
+                id: 3,
                 name: "Quesoburguesa Doble",
                 description: "Soda lata 355ml y papas medianas",
                 price: 6.45,
                 image: "../../../../assets/"
             },
             {
+                id: 4,
                 name: "Big Tasty",
                 description: "Soda lata 355ml y papas medianas",
                 price: 7.74,
@@ -38,27 +42,31 @@ export default[
         banner: require("../../../../assets/Pizza.png"),
         menu: [
             {
-                name: "Banquete Pizza Especial",
-                description: "",
+                id: 1,
+                name: "Banquete Pizza Especialidad",
+                description: "Pizza de Especialidad + un complemento",
                 price: 15.95,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 2,
+                name: "Banquete Hut Cheese",
+                description: "Pizza Hut Cheese (Orilla rellena de quezo mozzarella)",
+                price: 18.00,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 3,
+                name: "Dúo de Pizzas Grandes",
+                description: "2 Pizzas Grandes de 8 porciones cada una",
+                price: 22.00,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 4,
+                name: "Banquete Pizza Ingrediente",
+                description: "Pizza de 1 Ingrediente + un complemento",
+                price: 14.00,
                 image: "../../../../assets/"
             }
         ]
@@ -70,27 +78,31 @@ export default[
         banner: require("../../../../assets/china.png"),
         menu: [
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 1,
+                name: "Combinación 2 sabores de Pollo",
+                description: "Incluye dos sabores de pollo, un pan, un wantán y bebida.",
+                price: 8.25,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 2,
+                name: "Combinación 2 sabores de Pollo y Carne",
+                description: "Incluye una especialidad de carne y una de pollo, un pan, un wantán y bebida.",
+                price: 8.50,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 3,
+                name: "Combo Sopa Mein Lomito 24oz",
+                description: "Combo Sopa Mein Lomito 24oz",
+                price: 5.50,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 4,
+                name: "Pollo Favoritos",
+                description: "Incluye especialidad de pollo que más te guste, un pan, un wantán y bebidas",
+                price: 6.75,
                 image: "../../../../assets/"
             }
         ]
@@ -102,27 +114,31 @@ export default[
         banner: require("../../../../assets/burguer12.jpg"),
         menu: [
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 1,
+                name: "Whopper",
+                description: "Carne de res 4 oz. Asada a la parrilla y frescos vegetales.",
+                price: 6.99,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 2,
+                name: "Whopper doble",
+                description: "8 oz. De carne de res asada a la parrilla y frescos vegetales.",
+                price: 7.99,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 3,
+                name: "Big King",
+                description: "Doble carne de res 2oz. Queso americano, nuestra salsa stacker y vegetales.",
+                price: 4.99,
                 image: "../../../../assets/"
             },
             {
-                name: "",
-                description: "",
-                price: 0.00,
+                id: 4,
+                name: "SteakHouse",
+                description: "Carne de res de 7 oz. Asada a la parrilla, queso americano, cebolla crispy y vegetales",
+                price: 8.99,
                 image: "../../../../assets/"
             }
         ]
